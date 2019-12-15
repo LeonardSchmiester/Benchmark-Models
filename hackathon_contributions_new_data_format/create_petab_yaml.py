4d4b6758081a45ed76538952d28436f4321c4d73
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-12-15 21:10:00 +0100

    Handle special cases in YAML generation

--- hackathon_contributions_new_data_format/create_petab_yaml.py
+++ hackathon_contributions_new_data_format/create_petab_yaml.py
@@ -5,11 +5,15 @@ import pandas as pd
 import yaml
 import petab
 
+from copy import deepcopy
+
 
 def create_petab_yaml(model_dir):
-    data = {
+    filename = f'{model_dir}.yaml'
+    config = {
         'petab_version': petab.__version__,
-        'parameter_file': petab.get_default_parameter_file_name(model_dir),
+        'parameter_file':
+            petab.get_default_parameter_file_name(model_dir),
         'problems': [
             {
                 'sbml_file':
@@ -17,19 +21,47 @@ def create_petab_yaml(model_dir):
                 'condition_file':
                     petab.get_default_condition_file_name(model_dir),
                 'measurement_files':
-                [
-                    petab.get_default_measurement_file_name(model_dir),
-                ],
+                    [
+                        petab.get_default_measurement_file_name(model_dir),
+                    ],
             },
         ]
     }
 
-    petab.validate(data)
+    data = [
+        (filename, config),
+    ]
 
-    out_file = os.path.join(model_dir, f'{model_dir}.yaml')
-    print('git add ' + out_file)
-    with open(out_file, 'w') as outfile:
-        yaml.dump(data, outfile, default_flow_style=False)
+    if model_dir == 'Becker_Science2010':
+        config['problems'].append(deepcopy(config['problems'][0]))
+        config['problems'][0]['sbml_file'] = \
+            'model_Becker_Science2010__BaF3_Exp.xml'
+        config['problems'][0]['condition_file'] = \
+            'experimentalCondition_Becker_Science2010__BaF3_Exp.tsv'
+        config['problems'][0]['measurement_files'][0] = \
+            'measurementData_Becker_Science2010__BaF3_Exp.tsv'
+        config['problems'][1]['sbml_file'] = \
+            'model_Becker_Science2010__binding.xml'
+        config['problems'][1]['condition_file'] = \
+            'experimentalCondition_Becker_Science2010__binding.tsv'
+        config['problems'][1]['measurement_files'][0] = \
+            'measurementData_Becker_Science2010__binding.tsv'
+        # simulationData_Becker_Science2010__BaF3_Exp.tsv
+        # simulationData_Becker_Science2010__binding.tsv
+
+    elif model_dir in ('Casaletto_PNAS2019', 'Merkle_PCB2016'):
+        print(f"# ERROR: {model_dir}: model missing")
+        return
+
+
+
+    for filename, config in data:
+        petab.validate(config, wd=model_dir)
+
+        out_file = os.path.join(model_dir, filename)
+        print('git add ' + out_file)
+        with open(out_file, 'w') as outfile:
+            yaml.dump(data, outfile, default_flow_style=False)
 
 
 def main():
